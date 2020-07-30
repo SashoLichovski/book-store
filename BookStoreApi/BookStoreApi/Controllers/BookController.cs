92863cc40d3ecd7e528a45e01713ee32c5fa9db7
SashoLichovski <licovskiph@gmail.com>
2020-07-30 11:59:28 +0200

    BookController / Project arch
    Implemented BookController / Separated the solution on different projects

---
+++ BookStoreApi/BookStoreApi/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(new { Test = "test" });
+        }
+    }
+}
